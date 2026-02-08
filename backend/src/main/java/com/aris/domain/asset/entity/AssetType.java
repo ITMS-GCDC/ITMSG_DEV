106e6002e6b461cbd8ff62c0209ed037ff39e095
Symphonia <intel72440bx@gmail.com>
2026-02-08 15:38:38 +0900

    자산관리 데이터 생성건 수정

--- backend/src/main/java/com/aris/domain/asset/entity/AssetType.java
+++ backend/src/main/java/com/aris/domain/asset/entity/AssetType.java
@@ -4,12 +4,13 @@ package com.aris.domain.asset.entity;
  * 자산 유형
  */
 public enum AssetType {
-    PC,         // 데스크톱
+    // 노트북, 책상, 의자, 프린트, 복사기 , 기타 로 정의 26-02-08
     LAPTOP,     // 노트북
+    TABLE,      // 책상
+    CHAIR,      // 의자
     MONITOR,    // 모니터
-    SERVER,     // 서버
-    NETWORK,    // 네트워크 장비
     PRINTER,    // 프린터
+    DUPLICATOR,  // 복사기 
     OTHER       // 기타
 }
 
